9ebd77f0631c79c5b250307271ceed54b717e7d6
System Administrator <root@lm-sjn-00872516.paypalcorp.com>
2016-02-05 19:19:03 -0800

    Deployment script is complete. Working on ansible playbooks.

---
+++ deploy_stack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+IP="10.1.200.50"
+echo "Cruise control: Deploying the cluster, stand by..."
+vagrant destroy -f > /dev/null
+vagrant up 
+# Waiting for the final slave in the cluster
+
+  while [ `nc $IP 22 </dev/null|wc -l` -eq 0 ] 
+  do 
+echo "Waiting for slaves to be deployed"; sleep 5
+  done
+echo "Initiating provisioning using ansible...."
+ansible-playbook -i inventory messos-deploy.yml
+
